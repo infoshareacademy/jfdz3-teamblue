283d99a1864e91f4c9af17a21109a81955eb4898
Kajetan Mazur <kajetan@webownia.pl>
2017-02-14 19:17:50 +0100

    dodanie back to top

--- js/main.js
+++ js/main.js
@@ -201,7 +201,12 @@ $(function(){
 
     });
 
-
+//Kajetan
+    if ( ($(window).height() + 100) < $(document).height() ) {
+        $('#top-link-block').removeClass('hidden').affix({
+            offset: {top:100}
+        });
+    }
 
 
 
