e55e3c8d8c70e45eaf62a2ecb8ca8f787b68d7cc
Natalia Wiklent <natalia.wiklent@gmail.com>
2017-02-19 17:04:56 +0100

    poprawka js features do RWD

--- js/main.js
+++ js/main.js
@@ -1,14 +1,14 @@
-$(function(){
+$(function() {
 
 //Sylwia
     /* plynność menu */
-    $(document).ready(function() {
+    $(document).ready(function () {
 
-        $('a[href^="#"]').on('click', function(event) {
+        $('a[href^="#"]').on('click', function (event) {
 
-            var target = $( $(this).attr('href') );
+            var target = $($(this).attr('href'));
 
-            if( target.length ) {
+            if (target.length) {
                 event.preventDefault();
                 $('html, body').animate({
                     scrollTop: target.offset().top
@@ -19,206 +19,217 @@ $(function(){
     });
     /* zmienny tytul strony*/
 
-    $('li.about_us_menu').click(function()
-    {
-        document.title='Shop.pet - O produkcie';
+    $('li.about_us_menu').click(function () {
+        document.title = 'Shop.pet - O produkcie';
     });
 
-    $('li.features_menu').click(function()
-    {
-        document.title='Shop.pet - Funkcjonalnosci';
+    $('li.features_menu').click(function () {
+        document.title = 'Shop.pet - Funkcjonalnosci';
     });
 
-    $('li.dropdown').click(function()
-    {
-        document.title='Shop.pet - Nasz Zespol';
+    $('li.dropdown').click(function () {
+        document.title = 'Shop.pet - Nasz Zespol';
     });
-    $('li.newsletter_menu').click(function()
-    {
-        document.title='Shop.pet - Newsletter';
+    $('li.newsletter_menu').click(function () {
+        document.title = 'Shop.pet - Newsletter';
     });
-    $('li.more_about_menu').click(function()
-    {
-        document.title='Shop.pet - Napisali o nas';
+    $('li.more_about_menu').click(function () {
+        document.title = 'Shop.pet - Napisali o nas';
     });
-    $('li#shop').click(function()
-    {
-        document.title='Shop.pet - Sklep';
+    $('li#shop').click(function () {
+        document.title = 'Shop.pet - Sklep';
     });
-
 
 
 //Natalia
 
-    // 1
 
-    $('.feature_mobile').mouseover( function () {
-        $(this).stop(true, false).animate({
-            width: "20em",
-            height: "20em"
-        }, 0, function() {
-        });
+    var viewportWidth = $(window).width();
 
-        $('.feature_mobile img').stop(true, false).animate({
-            width: "19em",
-            height: "19em"
-        }, 0, function() {
-        });
+   console.log (viewportWidth)
 
-        $('.feature_mobile h2').stop(true, false).hide();
-
-        $('#icon1').stop(true, false).animate({display: "block"}, 0).fadeIn("slow");
-
-    });
+        if (viewportWidth > 620 ) {
 
 
-    $('.feature_mobile').mouseout( function () {
-        $(this).animate({
-            width: "18em",
-            height: "18em"
-        }, 0, function() {
-        });
 
-        $('.feature_mobile img').animate({
-            width: "17em",
-            height: "17em"
-        }, 0, function() {
-        });
+            // 1
 
-        $('.feature_mobile h2').stop(true, false).fadeIn( "slow", function() {
-        });
+            $('.feature_mobile').mouseover(function () {
+                $(this).stop(true, false).animate({
+                    width: "20em",
+                    height: "20em"
+                }, 0, function () {
+                });
 
-        $('#icon1').stop(true, false).animate({display: "none"}, 0).fadeOut(0);
+                $('.feature_mobile img').stop(true, false).animate({
+                    width: "19em",
+                    height: "19em"
+                }, 0, function () {
+                });
 
-    });
+                $('.feature_mobile h2').stop(true, false).hide();
 
-    //2
+                $('#icon1').stop(true, false).animate({display: "block"}, 0).fadeIn("slow");
 
-    $('.feature_wcag').mouseover( function () {
-        $(this).stop(true, false).animate({
-            width: "20em",
-            height: "20em"
-        }, 0, function() {
-        });
-
-        $('.feature_wcag img').stop(true, false).animate({
-            width: "19em",
-            height: "19em"
-        }, 0, function() {
-        });
-
-        $('.feature_wcag h2').stop(true, false).hide();
-
-        $('#icon2').stop(true, false).animate({display: "block"}, 0).fadeIn("slow");
-
-    });
+            });
 
 
-    $('.feature_wcag').mouseout( function () {
-        $(this).animate({
-            width: "18em",
-            height: "18em"
-        }, 0, function() {
-        });
+            $('.feature_mobile').mouseout(function () {
+                $(this).animate({
+                    width: "18em",
+                    height: "18em"
+                }, 0, function () {
+                });
 
-        $('.feature_wcag img').animate({
-            width: "17em",
-            height: "17em"
-        }, 0, function() {
-        });
+                $('.feature_mobile img').animate({
+                    width: "17em",
+                    height: "17em"
+                }, 0, function () {
+                });
 
-        $('.feature_wcag h2').stop(true, false).fadeIn( "slow", function() {
-        });
+                $('.feature_mobile h2').stop(true, false).fadeIn("slow", function () {
+                });
 
-        $('#icon2').stop(true, false).animate({display: "none"}, 0).fadeOut(0);
+                $('#icon1').stop(true, false).animate({display: "none"}, 0).fadeOut(0);
 
-    });
+            });
 
-    //3
+            //2
 
-    $('.feature_search').mouseover( function () {
-        $(this).stop(true, false).animate({
-            width: "20em",
-            height: "20em"
-        }, 0, function() {
-        });
+            $('.feature_wcag').mouseover(function () {
+                $(this).stop(true, false).animate({
+                    width: "20em",
+                    height: "20em"
+                }, 0, function () {
+                });
 
-        $('.feature_search img').stop(true, false).animate({
-            width: "19em",
-            height: "19em"
-        }, 0, function() {
-        });
+                $('.feature_wcag img').stop(true, false).animate({
+                    width: "19em",
+                    height: "19em"
+                }, 0, function () {
+                });
 
-        $('.feature_search h2').stop(true, false).hide();
+                $('.feature_wcag h2').stop(true, false).hide();
 
-        $('#icon3').stop(true, false).animate({display: "block"}, 0).fadeIn("slow");
-
-    });
+                $('#icon2').stop(true, false).animate({display: "block"}, 0).fadeIn("slow");
 
 
-    $('.feature_search').mouseout( function () {
-        $(this).animate({
-            width: "18em",
-            height: "18em"
-        }, 0, function() {
-        });
-
-        $('.feature_search img').animate({
-            width: "17em",
-            height: "17em"
-        }, 0, function() {
-        });
-
-        $('.feature_search h2').stop(true, false).fadeIn( "slow", function() {
-        });
-
-        $('#icon3').stop(true, false).animate({display: "none"}, 0).fadeOut(0);
-
-    });
-
-    //4
-
-    $('.feature_security').mouseover( function () {
-        $(this).stop(true, false).animate({
-            width: "20em",
-            height: "20em"
-        }, 0, function() {
-        });
-
-        $('.feature_security img').stop(true, false).animate({
-            width: "19em",
-            height: "19em"
-        }, 0, function() {
-        });
-
-        $('.feature_security h2').stop(true, false).hide();
-
-        $('#icon4').stop(true, false).animate({display: "block"}, 0).fadeIn("slow");
-
-    });
+            });
 
 
-    $('.feature_security').mouseout( function () {
-        $(this).animate({
-            width: "18em",
-            height: "18em"
-        }, 0, function() {
-        });
+            $('.feature_wcag').mouseout(function () {
+                $(this).animate({
+                    width: "18em",
+                    height: "18em"
+                }, 0, function () {
+                });
 
-        $('.feature_security img').animate({
-            width: "17em",
-            height: "17em"
-        }, 0, function() {
-        });
+                $('.feature_wcag img').animate({
+                    width: "17em",
+                    height: "17em"
+                }, 0, function () {
+                });
 
-        $('.feature_security h2').stop(true, false).fadeIn( "slow", function() {
-        });
+                $('.feature_wcag h2').stop(true, false).fadeIn("slow", function () {
+                });
 
-        $('#icon4').stop(true, false).animate({display: "none"}, 0).fadeOut(0);
+                $('#icon2').stop(true, false).animate({display: "none"}, 0).fadeOut(0);
 
-    });
+            });
 
-    /*antibot*/
+            //3
+
+            $('.feature_search').mouseover(function () {
+                $(this).stop(true, false).animate({
+                    width: "20em",
+                    height: "20em"
+                }, 0, function () {
+                });
+
+                $('.feature_search img').stop(true, false).animate({
+                    width: "19em",
+                    height: "19em"
+                }, 0, function () {
+                });
+
+                $('.feature_search h2').stop(true, false).hide();
+
+                $('#icon3').stop(true, false).animate({display: "block"}, 0).fadeIn("slow");
+
+            });
+
+
+            $('.feature_search').mouseout(function () {
+                $(this).animate({
+                    width: "18em",
+                    height: "18em"
+                }, 0, function () {
+                });
+
+                $('.feature_search img').animate({
+                    width: "17em",
+                    height: "17em"
+                }, 0, function () {
+                });
+
+                $('.feature_search h2').stop(true, false).fadeIn("slow", function () {
+                });
+
+                $('#icon3').stop(true, false).animate({display: "none"}, 0).fadeOut(0);
+
+            });
+
+            //4
+
+            $('.feature_security').mouseover(function () {
+                $(this).stop(true, false).animate({
+                    width: "20em",
+                    height: "20em"
+                }, 0, function () {
+                });
+
+                $('.feature_security img').stop(true, false).animate({
+                    width: "19em",
+                    height: "19em"
+                }, 0, function () {
+                });
+
+                $('.feature_security h2').stop(true, false).hide();
+
+                $('#icon4').stop(true, false).animate({display: "block"}, 0).fadeIn("slow");
+
+            });
+
+
+            $('.feature_security').mouseout(function () {
+                $(this).animate({
+                    width: "18em",
+                    height: "18em"
+                }, 0, function () {
+                });
+
+                $('.feature_security img').animate({
+                    width: "17em",
+                    height: "17em"
+                }, 0, function () {
+                });
+
+                $('.feature_security h2').stop(true, false).fadeIn("slow", function () {
+                });
+
+                $('#icon4').stop(true, false).animate({display: "none"}, 0).fadeOut(0);
+
+            });
+
+        }
+
+
+
+
+
+
+
+/*antibot*/
 
     var firstNumber = Math.floor((Math.random() * 10) + 1);
     var secondNumber = Math.floor((Math.random() * 100) + 1);
