b4ac4038b0298d8507015d4cbdf4c3f00247b38e
sistar1989 <szymanska.sylvia@gmail.com>
2017-02-19 09:19:47 +0100

    płynne menu

--- js/main.js
+++ js/main.js
@@ -1,8 +1,21 @@
 $(function(){
 
 //Sylwia
+    $(document).ready(function() {
 
+        $('a[href^="#"]').on('click', function(event) {
 
+            var target = $( $(this).attr('href') );
+
+            if( target.length ) {
+                event.preventDefault();
+                $('html, body').animate({
+                    scrollTop: target.offset().top
+                }, 1000);
+            }
+        });
+
+    });
 
 //Natalia
 
